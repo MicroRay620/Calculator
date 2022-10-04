8028d7a7069da4529433f712b4c80f35b51e252e
Ruby_LoveCODE <46199301+MicroRay620@users.noreply.github.com>
2022-10-04 12:17:47 -0400

    Update math-calculator.py

--- math-calculator.py
+++ math-calculator.py
@@ -2,7 +2,7 @@ print("I am your Mathmatical Assistance Helper or MAH!")
 print("Please tell me your name!")
 name = input("Tell yoru MAH what your name is:")
 print("Hello,", name)
-print("I go one step at a time.")
+print("I go one step at a time. Please keeo in mind, I am currently unable to do letter variables.")
 x = int(input("Your first number:"))
 function = input("What kind of equasion do you want?")
 y = int(input("Your second number:"))
@@ -11,13 +11,49 @@ if exponents == "no":
     if function == "multiplication":
         z = x * y
         print("Your multiplication is", z)
+    if function == "Multiplication":
+        z = x * y
+        print("Your multiplication is", z)
+    if function == "MULTIPLICATION":
+        z = x * y
+        print("Your multiplication is", z)
+    if function == "*":
+        z = x * y
+        print("Your multiplication is", z)
     if function == "division":
+        z = x / y
+        print("You Division answer is", z)
+    if function == "Division":
+        z = x / y
+        print("You Division answer is", z)
+    if function == "DIVISION":
+        z = x / y
+        print("You Division answer is", z)
+    if function == "/":
         z = x / y
         print("You Division answer is", z)
     if function == "addition":
         z = x + y
         print("Your addition answer is", z)
+    if function == "Addition":
+        z = x + y
+        print("Your addition answer is", z)
+    if function == "ADDITION"
+        z = x + y
+        print("Your addition answer is", z)
+    if function == "+"
+        z = x + y
+        print("Your addition answer is", z)
     if function == "subtraction":
+        z = x - y
+        print("Your subtraction answer is", z)
+    if function == "Subtraction":
+        z = x - y
+        print("Your subtraction answer is", z)
+    if function == "SUBTRACTION":
+        z = x - y
+        print("Your subtraction answer is", z)
+    if function == "-":
         z = x - y
         print("Your subtraction answer is", z)
 if exponents == "yes":
@@ -26,7 +62,31 @@ if exponents == "yes":
         z = x * y
         z = z ** e
         print("Your multiplication is", z)
+    if function == "Multiplication":
+        z = x * y
+        z = z ** e
+        print("Your multiplication is", z)
+    if function == "MULTIPLICATION":
+        z = x * y
+        z = z ** e
+        print("Your multiplication is", z)
+    if function == "*":
+        z = x * y
+        z = z ** e
+        print("Your multiplication is", z)
     if function == "division":
+        z = x / y
+        z = z ** e
+        print("You Division answer is", z)
+    if function == "Division":
+        z = x / y
+        z = z ** e
+        print("You Division answer is", z)
+    if function == "DIVISION":
+        z = x / y
+        z = z ** e
+        print("You Division answer is", z)
+    if function == "/"
         z = x / y
         z = z ** e
         print("You Division answer is", z)
@@ -34,7 +94,31 @@ if exponents == "yes":
         z = x + y
         z = z ** e
         print("Your addition answer is", z)
+    if function == "Addition":
+        z = x + y
+        z = z ** e
+        print("Your addition answer is", z)
+    if function == "ADDITIONAL":
+        z = x + y
+        z = z ** e
+        print("Your addition answer is", z)
+    if function == "+":
+        z = x + y
+        z = z ** e
+        print("Your addition answer is", z)
     if function == "subtraction":
+        z = x - y
+        z = z ** e
+        print("Your subtraction answer is", z)
+     if function == "Subtraction":
+        z = x - y
+        z = z ** e
+        print("Your subtraction answer is", z)
+     if function == "SUBTRACTION":
+        z = x - y
+        z = z ** e
+        print("Your subtraction answer is", z)
+     if function == "-":
         z = x - y
         z = z ** e
         print("Your subtraction answer is", z)
