95aab415a9d03894a3ace941bd23d4faa27b537d
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2022-10-10 12:57:21 -0400

    Update time-calculator.py

--- time-calculator.py
+++ time-calculator.py
@@ -4,15 +4,15 @@ if unit in {"seconds" or "Seconds" or "SECONDS"}:
     sec = int(input("How many seconds? "))
     conv = input("What do you want to convert seconds to? (Must be more than a second) ")
     if conv in {"minutes" or "Minutes" or "MINUTES"}:
-        min = sec / 60
+        min = sec // 60
         print(sec, "seconds, when converted to minutes is", min, "minutes")
     if conv in {"hours" or "Hours" or "HOURS"}:
-        hrs = sec / 120
+        hrs = sec // 120
         print(sec, "seconds, converted to hours, is", hrs, "hours")
     if conv in {"days" or "Days" or "DAYS"}:
-        hrs = sec / 120
+        hrs = sec // 120
         print (sec, "seconds converted into hours is", hrs, "hours")
-        days = hrs /24
+        days = hrs // 24
         print(hrs, "hours converted to days is", days, "days")
         if days >= 1:
             print("Which is a greater than or equal to 1 day")
