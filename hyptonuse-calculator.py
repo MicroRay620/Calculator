0fa3568db25b39a8c5de96ed4079486e79ed1f9d
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2022-10-10 08:18:57 -0400

    Add files via upload

---
+++ hyptonuse-calculator.py
@@ -0,0 +1,16 @@
+from math import sqrt
+#Ruby Rose (started on October 2022)
+print("Hello! You have gone to MAH's Hyptonuse Calculator!")
+hypot = input("You need square root? ")
+if hypot == "yes" and "Yes" and "YES" and "Y":
+    #add in multiple variable options
+    a = int(input("What is the number for the first side? "))
+    x = a ** 2
+    b = int(input("What is the number for the second side? "))
+    y = b ** 2
+    c = x + y
+    z = sqrt(c)
+    print(a, "was squared, getting", x)
+    print(b, "was squared, getting", y)
+    print(x, "was added with", y, "getting", c)
+    print(c, "was square rooted, ending with", z, "as the f")
